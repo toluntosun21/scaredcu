3742bbad378b70d945a3edf609037fdb5636f5b9
Tolun Tosun <toluntosun@sabanciuni.edu>
2024-10-17 20:59:59 +0300

    _basemul function update

--- scaredcu/lbc/selection_functions.py
+++ scaredcu/lbc/selection_functions.py
@@ -5,16 +5,21 @@ import cupy as _cu
 import numpy as _np
 
 
-def _make_basemul(reduction, reduce, words=None):
-    dtype_long = util.s22s(reduction.o_dtype)
-    def _basemul(c, guesses):
-        c_ = c[:,:].astype(dtype_long) if words is None else c[:, words].astype(dtype_long)
-        if reduce:
-            res = reduction.reduce(c_[:, _cu.newaxis, :] * guesses[_cu.newaxis, :, _cu.newaxis])
+class _BaseMul:
+
+    def __init__(self, reduction, reduce=True, words=None):
+        self.reduction = reduction
+        self.reduce = reduce
+        self.words = words
+        self.dtype_long = util.s22s(self.reduction.o_dtype)
+
+    def __call__(self, c, guesses):
+        c_ = c[:,:].astype(self.dtype_long) if self.words is None else c[:, self.words].astype(self.dtype_long)
+        if self.reduce:
+            res = self.reduction.reduce(c_[:, _cu.newaxis, :] * guesses[_cu.newaxis, :, _cu.newaxis])
         else:
             res = c_[:, _cu.newaxis, :] * guesses[_cu.newaxis, :, _cu.newaxis]
         return res
-    return _basemul
 
 
 class BaseMul:
@@ -24,7 +29,7 @@ class BaseMul:
     def __new__(cls, reduction, reduce=True, words=None, ciphertext_tag='c', key_tag='s'):
         return _decorated_selection_function(
             _AttackSelectionFunctionWrapped,
-            _make_basemul(reduction, reduce, words),
+            _BaseMul(reduction, reduce, words),
             expected_key_function=None,
             words=None,
             guesses=reduction.reduce(_cu.arange(reduction.q, dtype=reduction.o_dtype)),
@@ -40,7 +45,7 @@ class BaseMulLargeQ:
     def __new__(cls, reduction, cu_step, reduce=True, words=None, ciphertext_tag='c', key_tag='s'):
         return _decorated_selection_function(
             _IteratedAttackSelectionFunctionWrapped,
-            _make_basemul(reduction, reduce, words),
+            _BaseMul(reduction, reduce, words),
             expected_key_function=None,
             words=None,
             guesses=reduction.reduce(_np.arange(reduction.q, dtype=reduction.o_dtype)),
@@ -50,16 +55,15 @@ class BaseMulLargeQ:
             byte_guesses=False)
 
 
-def _make_basemul_incomplete(reduction, reduce, words=None):
-    dtype_long = util.s22s(reduction.o_dtype)
-    def _basemul_incomplete(c, guesses):
-        c_ = c[:,:].astype(dtype_long) if words is None else c[:, words].astype(dtype_long)
-        if reduce:
-            res = reduction.reduce((c_[:, _cu.newaxis, :, :] * guesses[_cu.newaxis, :, _cu.newaxis, :]).sum(axis=3))
+class _BaseMulIncomplete(_BaseMul):
+
+    def __call__(self, c, guesses):
+        c_ = c[:,:].astype(self.dtype_long) if self.words is None else c[:, self.words].astype(self.dtype_long)
+        if self.reduce:
+            res = self.reduction.reduce((c_[:, _cu.newaxis, :, :] * guesses[_cu.newaxis, :, _cu.newaxis, :]).sum(axis=3))
         else:
             res = (c_[:, _cu.newaxis, :, :] * guesses[_cu.newaxis, :, _cu.newaxis, :]).sum(axis=3)
         return res
-    return _basemul_incomplete
 
 
 class BaseMulIncomplete:
@@ -73,7 +77,7 @@ class BaseMulIncomplete:
         guesses = _cu.concatenate((low[:, _cu.newaxis], high[:, _cu.newaxis]), axis=1)
         return _decorated_selection_function(
             _AttackSelectionFunctionWrapped,
-            _make_basemul_incomplete(reduction, reduce),
+            _BaseMulIncomplete(reduction, reduce),
             expected_key_function=None,
             words=None,
             guesses=guesses,
