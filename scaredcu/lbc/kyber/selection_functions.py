73dbadbb7d51d80030561d6706eb12c844de3149
Tolun Tosun <toluntosun@sabanciuni.edu>
2024-10-24 23:56:46 +0300

    Class hierarchy improved for Kyber and its selection functions

--- scaredcu/lbc/kyber/selection_functions.py
+++ scaredcu/lbc/kyber/selection_functions.py
@@ -7,41 +7,34 @@ import cupy as _cu
 import numpy as _np
 
 
-MONTGOMERY = 0
-PLANTARD = 1
+class _BaseMul(selection_functions._BaseMul):
 
-class _KyberBaseMul(selection_functions._BaseMul):
-
-    def __init__(self, reduction=MONTGOMERY, low=False, high=True, reduce=True, words=None):
+    def __init__(self, basemul_imp=kyber.BaseMulMonty(), low=False, high=True, reduce=True, words=None):
         self.reduce = reduce
         self.words = words
         self.words_flat = _cu.tile(self.words, 2) * 2
         self.words_flat[1::2] += 1
         self.low = low
         self.high = high
-        if reduction == MONTGOMERY:
-            self.f = kyber.basemul_monty
-        elif reduction == PLANTARD:
-            self.f = kyber.basemul_plant
-        else:
-            raise ValueError(f'Unknown reduction method: {reduction}. Reduction method must be either "MONTGOMERY" or "PLANTARD".')
+        self.basemul_imp = basemul_imp
 
     def __call__(self, c, guesses):
         c_ = c[:,:].astype('int32') if self.words_flat is None else c[:, self.words_flat].astype('int32')
-        k = self.f(c_[:, _cu.newaxis, _cu.newaxis, :], guesses[_cu.newaxis, :, _cu.newaxis], self.words, self.low, self.high)
+        k = self.basemul_imp.basemul(c_[:, _cu.newaxis, _cu.newaxis, :], guesses[_cu.newaxis, :, _cu.newaxis], self.words, self.low, self.high)
         return k[:,:,0]
 
-class KyberBaseMul:
+
+class BaseMul:
     """Build an attack selection function which computes output values from the Kyber's base multiplication.
     """
 
-    def __new__(cls, reduce=True, words=None, low=True, high=False, guesses_low=_cu.arange(kyber.q, dtype='int16'), guesses_high=None, neg_trick=False, ciphertext_tag='c', key_tag='s'):
+    def __new__(cls, basemul_imp=kyber.BaseMulMonty(), reduce=True, words=None, low=True, high=False, guesses_low=_cu.arange(kyber.q, dtype='int16'), guesses_high=None, neg_trick=False, ciphertext_tag='c', key_tag='s'):
         if guesses_high is None:
             guesses_high = _np.arange(kyber.q, dtype='int16') if not neg_trick else _np.arange(kyber.q//2 + 1, dtype='int16')
         guesses = _cu.concatenate((_cu.tile(guesses_low, len(guesses_high))[:,_cu.newaxis], _cu.repeat(guesses_high, len(guesses_low))[:,_cu.newaxis]), axis=-1)
         return _decorated_selection_function(
             _AttackSelectionFunctionWrapped,
-            _KyberBaseMul(MONTGOMERY, low, high, reduce, words),
+            _BaseMul(basemul_imp, low, high, reduce, words),
             expected_key_function=None,
             words=None,
             guesses=guesses,
@@ -50,17 +43,27 @@ class KyberBaseMul:
             byte_guesses=False)
 
 
-class KyberBaseMulIterated:
+class BaseMulMonty(BaseMul):
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls, *args, basemul_imp=kyber.BaseMulMonty(), **kwargs)
+
+
+class BaseMulPlant(BaseMul):
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls, *args, basemul_imp=kyber.BaseMulPlant(), **kwargs)
+
+
+class BaseMulIterated:
     """Build an attack selection function which computes output values from the base multiplication.
     """
 
-    def __new__(cls, cu_step, reduce=True, words=None, low=True, high=False, guesses_low=_np.arange(kyber.q, dtype='int16'), guesses_high=None, neg_trick=False, ciphertext_tag='c', key_tag='s'):
+    def __new__(cls, cu_step, basemul_imp=kyber.BaseMulMonty(), reduce=True, words=None, low=True, high=False, guesses_low=_np.arange(kyber.q, dtype='int16'), guesses_high=None, neg_trick=False, ciphertext_tag='c', key_tag='s'):
         if guesses_high is None:
             guesses_high = _np.arange(kyber.q, dtype='int16') if not neg_trick else _np.arange(kyber.q//2 + 1, dtype='int16')
         guesses = _np.concatenate((_np.tile(guesses_low, len(guesses_high))[:,_np.newaxis], _np.repeat(guesses_high, len(guesses_low))[:,_np.newaxis]), axis=-1)        
         return _decorated_selection_function(
             _IteratedAttackSelectionFunctionWrapped,
-            _KyberBaseMul(MONTGOMERY, low, high, reduce, words),
+            _BaseMul(basemul_imp, low, high, reduce, words),
             expected_key_function=None,
             words=None,
             guesses=guesses,
@@ -69,33 +72,12 @@ class KyberBaseMulIterated:
             key_tag=key_tag,
             byte_guesses=False)
 
-# class KyberBaseMulPlant:
-#     """Build an attack selection function which computes output values from the Kyber's base multiplication.
-#     """
 
-#     def __new__(cls, reduction, reduce=True, words=None, low=True, high=False, ciphertext_tag='c', key_tag='s'):
-#         return _decorated_selection_function(
-#             _AttackSelectionFunctionWrapped,
-#             _KyberBaseMul('plantard', low, high, reduce, words),
-#             expected_key_function=None,
-#             words=None,
-#             guesses=reduction.reduce(_cu.arange(reduction.q, dtype=reduction.o_dtype)),
-#             target_tag=ciphertext_tag,
-#             key_tag=key_tag,
-#             byte_guesses=False)
+class BaseMulMontyIterated(BaseMulIterated):
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls, *args, basemul_imp=kyber.BaseMulMonty(), **kwargs)
 
 
-# class KyberBaseMulBarrett:
-#     """Build an attack selection function which computes output values from the Kyber's base multiplication.
-#     """
-
-#     def __new__(cls, reduction, reduce=True, words=None, low=True, high=False, ciphertext_tag='c', key_tag='s'):
-#         return _decorated_selection_function(
-#             _AttackSelectionFunctionWrapped,
-#             _BaseMul(reduction, reduce, words),
-#             expected_key_function=None,
-#             words=None,
-#             guesses=reduction.reduce(_cu.arange(reduction.q, dtype=reduction.o_dtype)),
-#             target_tag=ciphertext_tag,
-#             key_tag=key_tag,
-#             byte_guesses=False)
+class BaseMulPlantIterated(BaseMulIterated):
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls, *args, basemul_imp=kyber.BaseMulPlant(), **kwargs)
