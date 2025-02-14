4d2fb246261a9aa11a14be444aabc4d1d0fc0d78
Tolun Tosun <toluntosun@sabanciuniv.edu>
2025-02-14 14:11:52 +0300

    collision attack improved

--- scaredcu/analysis/collision_attack.py
+++ scaredcu/analysis/collision_attack.py
@@ -3,47 +3,18 @@ from scaredcu.distinguishers import CollisionDistinguisherMixin
 import cupy as _cp
 
 
-def _nanmean(arr, axis=None):
-    nan_mask = ~_cp.isnan(arr)
-    valid_count = _cp.sum(nan_mask, axis=axis)
-    total_sum = _cp.nansum(arr, axis=axis)
-    return total_sum / valid_count
-
-
 class CollisionAttack(BasePartitionedAttack, CollisionDistinguisherMixin):
     """Correlation enhanced collision attack.
 
     """
 
-    def __init__(self, offset, reverse_selection_function, attack_selection_function, **kwargs):
+    def __init__(self, offset, reverse_selection_function, delta_selection_function, **kwargs):
         super().__init__(selection_function=reverse_selection_function, model=None, **kwargs)
-        self.attack_selection_function = attack_selection_function
-        self.offset = offset
+        CollisionDistinguisherMixin.__init__(self, offset, delta_selection_function)
 
     def _set_model(self, model):
         pass
 
     def compute_intermediate_values(self, metadata):
-        self.saved_keys = metadata.keys()
+        self._set_delta_func_args(metadata.keys())
         return self.selection_function(**metadata)
-
-    def _compute(self):
-        p0_values = self.partitions
-        kwargs = {}
-        for key in self.saved_keys:
-            kwargs[key] = p0_values
-        p1_values = self.attack_selection_function(**kwargs)
-
-        key_index = 0 # for now
-
-        mean = self.sum / self.counters
-        for j in range(mean.shape[0]):
-            mean[j, _cp.isnan(mean[j])] = _nanmean(mean[j], axis=0).mean()
-
-        arr0 = mean[: mean.shape[0] - self.offset, key_index    , _cp.newaxis, :]
-        arr1 = mean[self.offset : mean.shape[0]  , key_index + 1, p1_values     ]
-
-        corr = _cp.empty((arr1.shape[1], 1, arr1.shape[0]), dtype=self.precision)
-        for i in range(corr.shape[2]):
-            corr[:, 0, i] = _cp.corrcoef(arr0[i], arr1[i], rowvar=False)[0, 1:]
-        return corr
--- scaredcu/distinguishers/partitioned.py
+++ scaredcu/distinguishers/partitioned.py
@@ -1,7 +1,7 @@
 from .base import DistinguisherMixin, _StandaloneDistinguisher
-import cupy as _cu
+import cupy as _cp
 import numba as _nb
-from numba import cuda as _cuda
+from numba import cuda as _cpda
 import time as _time
 import logging as _logging
 
@@ -12,12 +12,12 @@ class _PartitionnedDistinguisherBaseMixin(DistinguisherMixin):
 
     def _memory_usage(self, traces, data):
         self._init_partitions(data)
-        dtype_size = _cu.dtype(self.precision).itemsize
+        dtype_size = _cp.dtype(self.precision).itemsize
         return 3 * dtype_size * data.shape[1] * traces.shape[1] * len(self.partitions)
 
     def _init_partitions(self, data):
-        maxdata = _cu.nanmax(data)
-        mindata = _cu.nanmin(data)
+        maxdata = _cp.nanmax(data)
+        mindata = _cp.nanmin(data)
         if self.partitions is None:
             if maxdata > 255:
                 raise ValueError('max value for intermediate data is greater than 255, you need to provide partitions explicitly at init.')
@@ -27,7 +27,7 @@ class _PartitionnedDistinguisherBaseMixin(DistinguisherMixin):
             for r in ls:
                 if maxdata <= r:
                     break
-            self.partitions = _cu.arange(r, dtype='int32')
+            self.partitions = _cp.arange(r, dtype='int32')
 
     def _initialize(self, traces, data):
         self._trace_length = traces.shape[1]
@@ -40,7 +40,7 @@ class _PartitionnedDistinguisherBaseMixin(DistinguisherMixin):
             raise ValueError(f'traces has different length {traces.shape[1]} than already processed traces {self._trace_length}.')
         if data.shape[1] != self._data_words:
             raise ValueError(f'data has different number of data words {data.shape[1]} than already processed data {self._data_words}.')
-        if not _cu.issubdtype(data.dtype, _cu.integer):
+        if not _cp.issubdtype(data.dtype, _cp.integer):
             raise TypeError(f'data dtype for partitioned distinguisher, including MIA and Template, must be an integer dtype, not {data.dtype}.')
         logger.info(f'Update of partitioned distinguisher {self.__class__.__name__} in progress.')
         data = self._data_to_partition_index(data)
@@ -48,20 +48,20 @@ class _PartitionnedDistinguisherBaseMixin(DistinguisherMixin):
         logger.info(f'End of accumulations of traces for {self.__class__.__name__}.')
 
 
-@_cuda.jit()
+@_cpda.jit()
 def _build_lut(partitions, lut):
-    start = _cuda.grid(1)
-    stride = _cuda.gridsize(1)
+    start = _cpda.grid(1)
+    stride = _cpda.gridsize(1)
     for i in range(start, len(partitions), stride):
         lut[partitions[i]] = i
 
 
 def _define_lut_func(partitions):
-    lut = _cu.zeros(2**17, dtype='int32') - 1
+    lut = _cp.zeros(2**17, dtype='int32') - 1
     _build_lut[32,32](partitions, lut)
-    #@_cu.vectorize([_cu.int32(_cu.uint8), _cu.int32(_cu.uint16), _cu.int32(_cu.uint32), _cu.int32(_cu.uint64),
-    #                _cu.int32(_cu.int8), _cu.int32(_cu.int16), _cu.int32(_cu.int32), _cu.int32(_cu.int64)])
-    # @_cu.fuse
+    #@_cp.vectorize([_cp.int32(_cp.uint8), _cp.int32(_cp.uint16), _cp.int32(_cp.uint32), _cp.int32(_cp.uint64),
+    #                _cp.int32(_cp.int8), _cp.int32(_cp.int16), _cp.int32(_cp.int32), _cp.int32(_cp.int64)])
+    # @_cp.fuse
     def _lut_function(x):
         return lut[x]
 
@@ -83,16 +83,16 @@ class PartitionedDistinguisherMixin(_PartitionnedDistinguisherBaseMixin):
     """
 
     def _initialize_accumulators(self):
-        self.sum = _cu.zeros((self._trace_length, self._data_words, len(self.partitions)), dtype=self.precision)
-        self.sum_square = _cu.zeros((self._trace_length, self._data_words, len(self.partitions)), dtype=self.precision)
-        self.counters = _cu.zeros((self._data_words, len(self.partitions)), dtype=self.precision)
+        self.sum = _cp.zeros((self._trace_length, self._data_words, len(self.partitions)), dtype=self.precision)
+        self.sum_square = _cp.zeros((self._trace_length, self._data_words, len(self.partitions)), dtype=self.precision)
+        self.counters = _cp.zeros((self._data_words, len(self.partitions)), dtype=self.precision)
 
     @staticmethod
     @_nb.njit(parallel=True)
     def _accumulate_core_1(traces, data, self_sum, self_sum_square, self_counters, self_precision):
         for sample_idx in _nb.prange(traces.shape[1]):
-            tmp_sum = _cu.zeros((self_counters.shape[0], self_counters.shape[1]), dtype='float64')
-            tmp_sum_square = _cu.zeros((self_counters.shape[0], self_counters.shape[1]), dtype='float64')
+            tmp_sum = _cp.zeros((self_counters.shape[0], self_counters.shape[1]), dtype='float64')
+            tmp_sum_square = _cp.zeros((self_counters.shape[0], self_counters.shape[1]), dtype='float64')
             for trace_idx in range(traces.shape[0]):
                 x = traces[trace_idx, sample_idx]
                 xx = x * x
@@ -111,7 +111,7 @@ class PartitionedDistinguisherMixin(_PartitionnedDistinguisherBaseMixin):
     def _accumulate_core_2(traces, data, self_sum, self_sum_square, self_counters, self_precision):
         """Faster when number of partitions is <=9."""
         ftraces = traces.astype(self_precision)
-        bool_mask = _cu.empty((traces.shape[0], data.shape[1] * self_counters.shape[1]), dtype=self_precision)
+        bool_mask = _cp.empty((traces.shape[0], data.shape[1] * self_counters.shape[1]), dtype=self_precision)
         for p in range(self_counters.shape[1]):
             tmp_bool = data == p  # Data are already transformed to correspond to partition indexes.
             self_counters[:, p] += tmp_bool.sum(0)
@@ -129,7 +129,7 @@ class PartitionedDistinguisherMixin(_PartitionnedDistinguisherBaseMixin):
         else:
             if not hasattr(self, '_timings'):
                 self._timings = [-2, -1]
-            function_idx = _cu.argmin(self._timings)
+            function_idx = _cp.argmin(self._timings)
             function = [self._accumulate_core_1, self._accumulate_core_2][function_idx]
             t0 = _time.process_time()
             function(traces, data, self.sum, self.sum_square, self.counters, self.precision)
@@ -139,19 +139,19 @@ class PartitionedDistinguisherMixin(_PartitionnedDistinguisherBaseMixin):
         self.sum = self.sum.swapaxes(0, 1)
         self.sum_square = self.sum_square.swapaxes(0, 1)
 
-        result = _cu.empty((self._data_words, self._trace_length), dtype=self.precision)
+        result = _cp.empty((self._data_words, self._trace_length), dtype=self.precision)
 
         for i in range(self._data_words):
             non_zero_indices = self.counters[i] > 0
             non_zero_counters = self.counters[i][non_zero_indices]
             sums = self.sum[i][:, non_zero_indices]
             sums_squared = self.sum_square[i][:, non_zero_indices]
-            number_non_zero = _cu.sum(non_zero_counters)
+            number_non_zero = _cp.sum(non_zero_counters)
 
             tmp_result = self._compute_metric(
                 non_zero_indices, non_zero_counters, sums, sums_squared, number_non_zero
             )
-            tmp_result[_cu.isinf(tmp_result)] = _cu.nan
+            tmp_result[_cp.isinf(tmp_result)] = _cp.nan
             result[i] = tmp_result.astype(self.precision)
 
         self.sum = self.sum.swapaxes(0, 1)
@@ -162,16 +162,16 @@ class PartitionedDistinguisherMixin(_PartitionnedDistinguisherBaseMixin):
 
 def _set_partitions(obj, partitions):
     if partitions is not None:
-        if not isinstance(partitions, (_cu.ndarray, list, range)):
+        if not isinstance(partitions, (_cp.ndarray, list, range)):
             raise TypeError(f'partitions should be a ndarray, list or range instance, not {type(partitions)}.')
-        if not isinstance(partitions, _cu.ndarray):
-            partitions = _cu.array(partitions, dtype='int32')
+        if not isinstance(partitions, _cp.ndarray):
+            partitions = _cp.array(partitions, dtype='int32')
         elif partitions.dtype.kind not in 'iu':
             raise ValueError(f'partitions should be an integer array, not {partitions.dtype}.')
-        if _cu.max(partitions) >= 2**16:
-            raise ValueError(f'partition values must be in ]-2^16, 2^16[, but {_cu.max(partitions)} found.')
-        if _cu.min(partitions) <= -2**16:
-            raise ValueError(f'partition values must be in ]-2^16, 2^16[, but {_cu.min(partitions)} found.')
+        if _cp.max(partitions) >= 2**16:
+            raise ValueError(f'partition values must be in ]-2^16, 2^16[, but {_cp.max(partitions)} found.')
+        if _cp.min(partitions) <= -2**16:
+            raise ValueError(f'partition values must be in ]-2^16, 2^16[, but {_cp.min(partitions)} found.')
     obj.partitions = partitions
 
 
@@ -189,17 +189,17 @@ class ANOVADistinguisherMixin(PartitionedDistinguisherMixin):
     """This standalone partitioned distinguisher applies the ANOVA F-test metric."""
 
     def _compute_metric(self, non_zero_indices, non_zero_counters, sums, sums_squared, number_non_zero):
-        total_non_empty_partitions = _cu.count_nonzero(non_zero_indices)
+        total_non_empty_partitions = _cp.count_nonzero(non_zero_indices)
 
         partitions_means = (sums / non_zero_counters)
-        mean = _cu.sum(sums, axis=-1, keepdims=True) / number_non_zero
+        mean = _cp.sum(sums, axis=-1, keepdims=True) / number_non_zero
 
-        numerator = _cu.sum(
+        numerator = _cp.sum(
             (non_zero_counters * (partitions_means - mean) ** 2),
             axis=-1
         ) / (total_non_empty_partitions - 1)
 
-        denominator = _cu.sum(
+        denominator = _cp.sum(
             (sums_squared - sums ** 2 / non_zero_counters),
             axis=-1
         ) / (number_non_zero - total_non_empty_partitions)
@@ -219,13 +219,13 @@ class NICVDistinguisherMixin(PartitionedDistinguisherMixin):
     """This standalone partitioned distinguisher applies the NICV (Normalized Inter-Class Variance) metric."""
 
     def _compute_metric(self, non_zero_indices, non_zero_counters, sums, sums_squared, number_non_zero):
-        mean = _cu.sum(sums, axis=1) / number_non_zero
+        mean = _cp.sum(sums, axis=1) / number_non_zero
 
         numerator = (((sums / non_zero_counters).T - mean).T)**2
         numerator *= non_zero_counters / number_non_zero
-        numerator = _cu.sum(numerator, axis=1)
+        numerator = _cp.sum(numerator, axis=1)
 
-        denominator = _cu.sum(sums_squared, axis=1) / number_non_zero - (mean)**2
+        denominator = _cp.sum(sums_squared, axis=1) / number_non_zero - (mean)**2
 
         return numerator / denominator
 
@@ -242,12 +242,12 @@ class SNRDistinguisherMixin(PartitionedDistinguisherMixin):
     """This standalone partitioned distinguisher applies the SNR (Signal to Noise Ratio) metric."""
 
     def _compute_metric(self, non_zero_indices, non_zero_counters, sums, sums_squared, number_non_zero):
-        mean = _cu.sum(sums, axis=1) / number_non_zero
+        mean = _cp.sum(sums, axis=1) / number_non_zero
         numerator = (((sums / non_zero_counters).T - mean).T)**2
-        numerator = _cu.sum(numerator, axis=1) / non_zero_indices.shape[0]
+        numerator = _cp.sum(numerator, axis=1) / non_zero_indices.shape[0]
 
         denominator = (sums_squared / non_zero_counters) - (sums / non_zero_counters)**2
-        denominator = _cu.sum(denominator, axis=1) / non_zero_indices.shape[0]
+        denominator = _cp.sum(denominator, axis=1) / non_zero_indices.shape[0]
 
         return numerator / denominator
 
@@ -260,21 +260,32 @@ class SNRDistinguisher(PartitionedDistinguisherBase, SNRDistinguisherMixin):
     __doc__ = PartitionedDistinguisherMixin.__doc__ + SNRDistinguisherMixin.__doc__
 
 
+def _nanmean(arr, axis=None):
+    nan_mask = ~_cp.isnan(arr)
+    valid_count = _cp.sum(nan_mask, axis=axis)
+    total_sum = _cp.nansum(arr, axis=axis)
+    return total_sum / valid_count
+
 
 class CollisionDistinguisherMixin(_PartitionnedDistinguisherBaseMixin):
     """DistinguisherMixin for collision attacks."""
 
+    def __init__(self, offset, delta_selection_function):
+        self.offset = offset
+        self.delta_selection_function = delta_selection_function
+        self._saved_keys = None
+
     def _initialize_accumulators(self):
         if self._data_words != 2:
             raise ValueError('Collision distinguisher can only be used with 2 data words.')
-        self.sum = _cu.zeros((self._trace_length, self._data_words, len(self.partitions)), dtype=self.precision)
-        self.counters = _cu.zeros((self._data_words, len(self.partitions)), dtype=self.precision)
+        self.sum = _cp.zeros((self._trace_length, self._data_words, len(self.partitions)), dtype=self.precision)
+        self.counters = _cp.zeros((self._data_words, len(self.partitions)), dtype=self.precision)
 
     @staticmethod
-    @_cuda.jit(cache=True)
+    @_cpda.jit(cache=True)
     def _accumulate_core(traces, data, self_sum, self_counters):
-        start = _cuda.grid(1)
-        stride = _cuda.gridsize(1)
+        start = _cpda.grid(1)
+        stride = _cpda.gridsize(1)
         for sample_idx in range(start, traces.shape[1], stride):
             for trace_idx in range(traces.shape[0]):
                 x = traces[trace_idx, sample_idx]
@@ -288,8 +299,34 @@ class CollisionDistinguisherMixin(_PartitionnedDistinguisherBaseMixin):
     def _accumulate(self, traces, data):
         self._accumulate_core[128,8](traces, data, self.sum, self.counters)
 
+    def _set_delta_func_args(self, keys):
+        self._saved_keys = keys
+
     def _compute(self):
-        pass
+
+        if self._saved_keys is None:
+            raise ValueError('No metadata provided for the delta selection function.')
+
+        p0_values = self.partitions
+
+        kwargs = {}
+        for key in self._saved_keys:
+            kwargs[key] = p0_values
+        p1_values = self.delta_selection_function(**kwargs)
+
+        key_index = 0 # for now
+
+        mean = self.sum / self.counters
+        for j in range(mean.shape[0]):
+            mean[j, _cp.isnan(mean[j])] = _nanmean(mean[j], axis=0).mean()
+
+        arr0 = mean[: mean.shape[0] - self.offset, key_index    , _cp.newaxis, :]
+        arr1 = mean[self.offset : mean.shape[0]  , key_index + 1, p1_values     ]
+
+        corr = _cp.empty((arr1.shape[1], 1, arr1.shape[0]), dtype=self.precision)
+        for i in range(corr.shape[2]):
+            corr[:, 0, i] = _cp.corrcoef(arr0[i], arr1[i], rowvar=False)[0, 1:]
+        return corr
 
     @property
     def _distinguisher_str(self):
